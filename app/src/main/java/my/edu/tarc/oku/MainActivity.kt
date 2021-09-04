5ccb0366a737a0a8e114743d8ec3cae6a1b52b94
ChinXin <68983548+ChinXin@users.noreply.github.com>
2021-09-05 00:29:04 +0800

    side bar

--- app/src/main/java/my/edu/tarc/oku/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/oku/MainActivity.kt
@@ -2,10 +2,97 @@ package my.edu.tarc.oku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.Menu
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import my.edu.tarc.oku.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        val headerView = navView.getHeaderView(0);
+        val btnSul: Button = headerView.findViewById(R.id.btnLoginSignUp)
+
+        btnSul.setOnClickListener(){
+            findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.loginFragment)
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.aboutUs, R.id.loginFragment, R.id.registerFragment
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.navdrawer_menu, menu)
+//        return true
+//    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+    }
+//    private lateinit var drawerLayout: DrawerLayout
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        @Suppress("UNUSED_VARIABLE")
+//        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+//
+//        drawerLayout = binding.drawerLayout
+//
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//
+//        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+//
+//        NavigationUI.setupWithNavController(binding.navView, navController)
+//
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//        return NavigationUI.navigateUp(navController, drawerLayout)
+//    }
 }
