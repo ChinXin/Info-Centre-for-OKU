f3c3150b37d9346d7951746ad47027be9d49b338
aeronchow <68983153+aeronchow@users.noreply.github.com>
2021-09-08 11:37:13 +0800

    info window

---
+++ app/src/main/java/my/edu/tarc/oku/CustomInfoWindowForGoogleMap.kt
@@ -0,0 +1,38 @@
+package my.edu.tarc.oku
+
+import android.app.Activity
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+
+class CustomInfoWindowForGoogleMap(context: Context) : GoogleMap.InfoWindowAdapter {
+
+    var mContext = context
+    var mWindow = (context as Activity).layoutInflater.inflate(R.layout.info_window, null)
+
+    private fun rendowWindowText(marker: Marker, view: View){
+
+        val tvTitle = view.findViewById<TextView>(R.id.title)
+        val tvSnippet = view.findViewById<TextView>(R.id.snippet)
+
+        tvTitle.text = marker.title
+        tvSnippet.text = marker.snippet
+
+    }
+
+    override fun getInfoContents(marker: Marker): View {
+        rendowWindowText(marker, mWindow)
+        return mWindow
+    }
+
+    override fun getInfoWindow(marker: Marker): View? {
+        rendowWindowText(marker, mWindow)
+        return mWindow
+    }
+}
--- app/src/main/java/my/edu/tarc/oku/HomeAdminFragment.kt
+++ app/src/main/java/my/edu/tarc/oku/HomeAdminFragment.kt
@@ -1,5 +1,7 @@
 package my.edu.tarc.oku
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.content.res.Resources
 import androidx.fragment.app.Fragment
 
@@ -8,24 +10,35 @@ import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class HomeAdminFragment : Fragment() {
 
     private lateinit var map: GoogleMap
-    private val TAG = HomeAdminFragment::class.java.simpleName
-    //private val REQUEST_LOCATION_PERMISSION = 1
+    //private val TAG = HomeAdminFragment::class.java.simpleName
+    private val REQUEST_LOCATION_PERMISSION = 1
 
     private val callback = OnMapReadyCallback { googleMap ->
         val sydney = LatLng(4.5447595, 101.1049368)
         map = googleMap
+
+        //optional style map
+        //setMapStyle(map)
+        enableMyLocation()
+        setMapLongClick(map)
+        map.setInfoWindowAdapter(CustomInfoWindowForGoogleMap(this.requireContext()))
+
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
@@ -37,10 +50,6 @@ class HomeAdminFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home_admin, container, false)
     }
 
-    //need modify
-//    override fun onMapReady(googleMap: GoogleMap){
-//        setMapStyle(googleMap)
-//    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
@@ -68,44 +77,75 @@ class HomeAdminFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-//        // Change the map type based on the user's selection.
-//        R.id.normal_map -> {
-//            map.mapType = GoogleMap.MAP_TYPE_NORMAL
-//            true
-//        }
-//        R.id.hybrid_map -> {
-//            map.mapType = GoogleMap.MAP_TYPE_HYBRID
-//            true
-//        }
-//        R.id.satellite_map -> {
-//            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
-//            true
-//        }
-//        R.id.terrain_map -> {
-//            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
-//            true
-//        }
-//        else -> super.onOptionsItemSelected(item)
-//    }
-//
-    private fun setMapStyle(map: GoogleMap) {
-        try {
-            // Customize the styling of the base map using a JSON object defined
-            // in a raw resource file.
-            val success = map.setMapStyle(
-                MapStyleOptions.loadRawResourceStyle(
-                    context,
-                    R.raw.map_style
-                )
-            )
+    private fun isPermissionGranted() : Boolean {
+        return ContextCompat.checkSelfPermission(
+            this.requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
 
-            if (!success) {
-                Log.e(TAG, "Style parsing failed.")
-            }
-        } catch (e: Resources.NotFoundException) {
-            Log.e(TAG, "Can't find style. Error: ", e)
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            map.isMyLocationEnabled = true
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                this.requireActivity(),
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
         }
     }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            // A Snippet is Additional text that's displayed below the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+            val test = map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))
+                    .snippet(snippet)
+                    .draggable(true)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+            )
+            test.showInfoWindow()
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+//    private fun setMapStyle(map: GoogleMap) {
+//        try {
+//            // Customize the styling of the base map using a JSON object defined
+//            // in a raw resource file.
+//            val success = map.setMapStyle(
+//                MapStyleOptions.loadRawResourceStyle(
+//                    context,
+//                    R.raw.map_style
+//                )
+//            )
+//
+//            if (!success) {
+//                Log.e(TAG, "Style parsing failed.")
+//            }
+//        } catch (e: Resources.NotFoundException) {
+//            Log.e(TAG, "Can't find style. Error: ", e)
+//        }
+//    }
 
 }
